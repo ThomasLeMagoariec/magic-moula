cdb789b65b15704b0035069545aff89f9c1d0317
Thomas <thomas.lemago@gmail.com>
2024-03-13 17:55:51 +0100

    changed stuff to english

--- webserver/magic-moula/src/pages/Login.jsx
+++ webserver/magic-moula/src/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
         </div>
         <div class="border border-2 rounded-3 form-floating input-group-lg mt-2 mb-2">
           <input id='password' type="password" placeholder="Mot de passe" className="form-control" />
-          <label for="password" class="form-label">Mot de passe</label>
+          <label for="password" class="form-label">Password</label>
         </div>
         </div>
         <div className='d-grid gap-2 col-9 mx-auto'>
@@ -25,7 +25,7 @@ const Login = () => {
         </div>
         <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%'}}>
           <div className='rounded-4' style={{height: '5px', width: '35%', backgroundColor: '#283044'}}></div>
-          <h1 color='#283044'>ou</h1>
+          <h1 color='#283044'>or</h1>
           <div className='rounded-4' style={{height: '5px', width: '35%', backgroundColor: '#283044'}}></div>
         </div>
         <div className='d-grid gap-2 col-9 mx-auto'>
