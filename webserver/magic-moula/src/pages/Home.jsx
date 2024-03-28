f1afb87a35f4c466afebd3fa08d1f822c62c18dc
Thomas <thomas.lemago@gmail.com>
2024-03-28 10:15:50 +0100

    malice

--- webserver/magic-moula/src/pages/Home.jsx
+++ webserver/magic-moula/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import farfadet from '../img/farfadet.png';
 
 const Home = () => {
     const navigate = useNavigate();
@@ -11,7 +12,7 @@ const Home = () => {
     <>
       <Header />
       <div style={{position: 'absolute', left: 0, bottom:0}}>
-      <img src="../../dist/assets/farfadet.png" style={{scale: "0.3", position: "sticky", bottom: 0, left: 0, zIndex: 10, transform: "translate(-100%, 100%)", cursor: "pointer"}} onClick={() => alert("Je suis un farfadet malicieux, rempli d'espièglerie!!")} />
+      <img src={farfadet} style={{scale: "0.3", position: "sticky", bottom: 0, left: 0, zIndex: 10, transform: "translate(-100%, 100%)", cursor: "pointer"}} onClick={() => alert("Je suis un farfadet malicieux, rempli d'espièglerie!!")} />
       </div>
       <div style={{display: 'flex', height: '100vh', background: "linear-gradient(to right, #00dbde, #fc00ff)"}}>
         <h2 style={{margin: "100px"}}>Bienvenue chez Magic Moula, votre banque enchantée!</h2>
