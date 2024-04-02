bf95a3ff60ea67afc30cb0c952a31760a1c1cab8
Huguette42 <jeanselme.hugo@ndbellegarde.net>
2024-04-02 08:14:14 +0200

    fix aberant

--- webserver/magic-moula/src/pages/Paiement.jsx
+++ webserver/magic-moula/src/pages/Paiement.jsx
@@ -15,23 +15,61 @@ const Paiement = () => {
     const params = new URLSearchParams(search);
     const amount = params.get('amount');
     const command = JSON.parse(params.get('command'));
+    const [balance, setBalance] = useState(0.00)
     console.log(command)
+    useEffect(() => {
+        fetch("http://localhost:5000/get_balance", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': localStorage.getItem('token')
+            }
+        }).then((response) => {
+            if (response.status === 200) {
+                return response.json()
+            }
+        }).then((data) => {
+            if (data) {
+                setBalance(data)
+            }
+        });
+    }, [])
+
     const handlelogin = () => {
         if (localStorage.getItem('connected') === 'true') {
+          const taxprice = +amount+ +amount*0.69;
             return (<>
-            <div className="row">
-              <div className="col">
+            <div className="row" style={{maxWidth: '100vw'}}>
+              <div className="col" style={{display: "flex",minHeight: '69.3vh', padding: '40px',alignItems: 'center', justifyContent: 'center'}}>
+                <div style={{position: 'relative',width: '55%',boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px", padding: '30px',textAlign:'center'}}>
             {command.map((item) => {
-                return <div key={item.name}>
+                return <div key={item.name} style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
                   
-                    <h1>{item.name}</h1>
+                    <h1 style={{fontSize: '2.5rem',fontFamily: 'Receipt'}}>{item.name}</h1>
+                    <h1 style={{fontSize: '2.5rem',fontFamily: 'Receipt'}}>{item.count+"x"+" "+item.price*item.count} €</h1>
                 </div>
             })}
+            <h1 style={{fontSize: '2.5rem',fontFamily: 'Receipt'}}>---------------------</h1>
+            <div style={{ display: 'flex', flexDirection: 'row',justifyContent: 'space-between'}}>
+              <h1 style={{fontSize: '2.5rem',fontFamily: 'Receipt'}}>Total HT</h1>
+              <h1 style={{fontSize: '2.5rem',fontFamily: 'Receipt'}}>{amount+" €"}</h1>
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'row',justifyContent: 'space-between'}}>
+              <h1 style={{fontSize: '2.5rem',fontFamily: 'Receipt'}}>Farfadet's Tax (69%)</h1>
+              <h1 style={{fontSize: '2.5rem',fontFamily: 'Receipt'}}>{(amount*0.69).toFixed(2)} €</h1>
+            </div>
+            <h1 style={{fontSize: '2.5rem',fontFamily: 'Receipt'}}>---------------------</h1>
+            <div style={{ display: 'flex', flexDirection: 'row',justifyContent: 'space-between'}}>
+              <h1 style={{fontSize: '2.5rem',fontFamily: 'Receipt'}}>Total</h1>
+              <h1 style={{fontSize: '2.5rem',fontFamily: 'Receipt'}}>{taxprice} €</h1>
+            </div>
+            </div>
             </div>
             <div className="col">
+              <h1 style={{fontSize: '2rem',}}>Your balance: {balance} €</h1>
                     <button className="btn btn-success">Pay</button>
-                  </div>
-                  </div>
+              </div>
+            </div>
             </>)
         } else {
             return <LoginComp />
@@ -42,9 +80,7 @@ const Paiement = () => {
   return (
     <>
     <Header />
-    <div>
       {handlelogin()}
-    </div>
     <Footer />
     </>
   );
